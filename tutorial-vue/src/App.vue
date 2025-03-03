983402ac2f796d9ad5e3f604440391f3402d0646
freddys <freddysz@gmail.com>
2025-03-03 10:59:41 +0100

    API

--- tutorial-vue/src/App.vue
+++ tutorial-vue/src/App.vue
@@ -1,9 +1,6 @@
 <!-- App.vue -->
 <template>
-  <div
-    id="app"
-    class="container"
-  >
+  <div id="app" class="container">
     <div class="row">
       <div class="col-md-12">
         <h1>Personas</h1>
@@ -13,11 +10,8 @@
       <div class="col-md-12">
         <!-- Inclusion del componente "TablaPersonas" -->
         <formulario-persona @add-persona="agregarPersona" />
-        <tabla-personas
-          :personas="personas"
-          @delete-persona="eliminarPersona"
-          @actualizar-persona="actualizarPersona"
-        />
+        <tabla-personas :personas="personas" @delete-persona="eliminarPersona"
+          @actualizar-persona="actualizarPersona" />
       </div>
     </div>
   </div>
@@ -27,7 +21,7 @@
 // Importacion del componente 'TablaPersonas' y el metodo 'ref' de Vue 3
 import TablaPersonas from "@/components/TablaPersonas.vue";
 import FormularioPersona from "@/components/FormularioPersona.vue";
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 // Definicion del componente Vue
 defineOptions({
   // Nombre del componente
@@ -36,31 +30,59 @@ defineOptions({
 // Declaracion de una variable reactiva "personas" usando "ref"
 const personas = ref([]);
 
-const agregarPersona = (persona) => {
-  let id = 0;
-  if (personas.value.length > 0) {
-    id = personas.value[personas.value.length - 1].id + 1;
-  }
-  personas.value = [...personas.value, { ...persona, id }];
-};
 
-const eliminarPersona = (id) => {
+const listadoPersonas = async () => {
+  // Metodo para obtener un listado de personas
   try {
-    personas.value = personas.value.filter((u) => u.id !== id);
+    const response = await fetch('https://my-json-server.typicode.com/rmarabini/people/personas/');
+    personas.value = await response.json();
   } catch (error) {
     console.error(error);
   }
 };
-
-const actualizarPersona = (id, personaActualizada) => {
+const agregarPersona = async (persona) => {
   try {
-    personas.value = personas.value.map((persona) =>
-      persona.id === id ? personaActualizada : persona
-    );
+    const response = await fetch('https://my-json-server.typicode.com/rmarabini/people/personas/', {
+      method: 'POST',
+      body: JSON.stringify(persona),
+      headers: { 'Content-type': 'application/json; charset=UTF-8' },
+    });
+    const personaCreada = await response.json();
+    personas.value = [...personas.value, personaCreada];
   } catch (error) {
     console.error(error);
   }
 };
+const eliminarPersona = async (persona_id) => {
+  // Metodo para eliminar una persona
+  try {
+    await fetch('https://my-json-server.typicode.com/rmarabini/people/personas/' + persona_id + '/', {
+      method: "DELETE"
+    });
+    personas.value = personas.value.filter(u => u.id !== persona_id);
+  } catch (error) {
+    console.error(error);
+  }
+};
+const actualizarPersona = async (id, personaActualizada) => {
+  // Metodo para actualizar una persona
+  try {
+    const response = await fetch('https://my-json-server.typicode.com/rmarabini/people/personas/' + personaActualizada.id + '/', {
+      method: 'PUT',
+      body: JSON.stringify(personaActualizada),
+      headers: { 'Content-type': 'application/json; charset=UTF-8' },
+    });
+    const personaActualizadaJS = await response.json();
+    personas.value = personas.value.map(u => (u.id === personaActualizada.id ? personaActualizadaJS : u));
+  } catch (error) {
+    console.error(error);
+  }
+};
+// Fetch data when the component is mounted
+onMounted(() => {
+  listadoPersonas();
+});
+
 </script>
 
 <style>
